b80380b3006d9d9174fbe50a7c1e96465cde42d8
wnowak8 <61703962+wnowak8@users.noreply.github.com>
2022-06-12 16:42:48 +0200

    Update utils.py

---
+++ backend/models/services/utils.py
@@ -0,0 +1,11 @@
+article_columns = ["id",
+                   "title",
+                   "content",
+                   "like_count",
+                   "dislike_count",
+                   "author",
+                   "created_at",
+                   "modified_at"]
+
+category_columns=["id","name","created_at","modified_at"]
+
