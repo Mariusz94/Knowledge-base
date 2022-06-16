3fe107265740075dbb2eda1d85cb81ef7309d8af
wnowak8 <61703962+wnowak8@users.noreply.github.com>
2022-06-16 14:18:14 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import logging
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from flask_restful import Api, Resource, reqparse
 from models.db.postgresDB import PostgresDB
 from models.services.logger import get_module_logger
 import models.services.flask_service as flask_service
@@ -12,18 +12,24 @@ load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app, resources=r'/*')
-
+parser = reqparse.RequestParser()
+parser.add_argument('keywords', type=list)
 
 @app.route('/', methods=['GET'])
 def hello_server():
     return jsonify({"info": "Server works"}), 200
 
-
 @app.route('/articles', methods=['GET'])
 def get_articles():
     article_id = request.args.get("article_id", None)
     return flask_service.get_articles(db=postgresDB, article_id=article_id)
+
+#TODO: z parametrem
+# @app.route('/articles', methods=['GET'])
+# def get_articles():
+#     keywords = parser.parse_args()
+#     return keywords
+#     #return flask_service.get_articles(db=postgresDB, keywords=keywords)
 
 
 @app.route('/articles', methods=['POST'])
@@ -63,6 +69,28 @@ def update_categories(category_id):
 @app.route('/categories/<category_id>', methods=['DELETE'])
 def delete_categories(category_id):
     return flask_service.delete_category(db=postgresDB, category_id=category_id,category_table=category_table)
+
+@app.route('/comments', methods=['GET'])
+def get_comment():
+    article_id = request.args.get("article_id", None)
+    author=request.args.get("author", None)
+    return flask_service.get_comments(db=postgresDB, article_id=article_id,author=author,comment_table=comment_table)
+
+
+@app.route('/comments', methods=['POST'])
+def create_comments():
+    data = request.json
+    return flask_service.create_comment(db=postgresDB, data=data)
+
+
+@app.route('/comments/<comment_id>', methods=['PUT'])
+def update_comments(comment_id):
+    data = request.json
+    return flask_service.update_comment(db=postgresDB, comment_id=comment_id, data=data)
+
+@app.route('/comments/<comment_id>', methods=['DELETE'])
+def delete_comments(comment_id):
+    return flask_service.delete_comment(db=postgresDB, comment_id=comment_id,comment_table=comment_table)
 
 if __name__ == "__main__":
     logger = get_module_logger(mod_name=__name__, log_path='./logs/app_logs.log', lvl=logging.DEBUG)
